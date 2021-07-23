05c1e1dd4990c73d582978835c148f60f29b8d7b
Debabrata-bhakat <bhakat.debabrata@gmail.com>
2021-07-23 21:23:21 +0530

    23JUL_EVE2

---
+++ ds_algo/8.lecture/StudentUse.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+#include "Student.cpp"
+
+int main(){
+    // Create objects statically
+    Student s1;
+    Student s2;
+
+    Student s3, s4, s5;
+
+    // Create objects dynamically
+    Student *s6 = new Student;
+    
+}
