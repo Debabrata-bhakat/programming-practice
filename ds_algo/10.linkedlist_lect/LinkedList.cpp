346f34b547698c81a07311be45acc34784563cfb
Debabrata-bhakat <bhakat.debabrata@gmail.com>
2021-07-30 22:49:32 +0530

    30Jul_night

--- ds_algo/10.linkedlist_lect/LinkedList.cpp
+++ ds_algo/10.linkedlist_lect/LinkedList.cpp
@@ -2,6 +2,11 @@
 using namespace std;
 #include "Node.cpp"
 
-int main{
-    :x
+int main(){
+    //statically
+    Node n1(1);
+    Node *head = &n1;
+    Node n2(2); 
+    n1.next = &n2;
+    cout << n1.data << " " << n2.data << endl;
 }
