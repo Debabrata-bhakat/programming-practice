578e972db3391d448e6085746f43c2fbbf8bc616
Debabrata-bhakat <bhakat.debabrata@gmail.com>
2021-08-11 22:56:31 +0530

    11Aug_night

--- ds_algo/12.stacks/StackUsingLL.cpp
+++ ds_algo/12.stacks/StackUsingLL.cpp
@@ -1,23 +1,44 @@
 #include <iostream>
 using namespace std;
 
+template <typename T>
+
 class Node{
     public:
-    int data;
-    Node *next;
+    T data;
+    Node<T> *next;
 
-    Node(int data){
+    Node(T data){
         this ->data = data;
         next = NULL;
     }
 };
 
+template <typename T>
 class Stack{
-    Node *head;
+    Node<T> *head;
     int size;
 
     public:
     Stack(){
+        head = NULL;
+        size = 0;
+    }
+
+    int getSize(){
+        return size;
+    }
+
+    void push(T element){
+        Node<T> *newNode = new Node<T>(element);
+        
+    }
+
+    T pop(){
+
+    }
+
+    T top(){
 
     }
 
